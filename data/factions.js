b25474c04e657de1871d4ba3c720410830656f11
Maximus7474 <the.maximus.7474@gmail.com>
2024-11-29 17:47:15 +0100

    chore: reindented factions.js

--- data/factions.js
+++ data/factions.js
@@ -1,97 +1,96 @@
 export default {
-    // ADF
-    "3rd Battalion, Royal Australian Regiment": { faction: "ADF", type: "Air assault" },
-    "3rd Brigade Battle Group": { faction: "ADF", type: "Combined Arms" },
-    "1st Battalion, Royal Australian Regiment": { faction: "ADF", type: "Mechanized" },
-  
-    // BAF
-    "2nd Battalion, Parachute Regiment": { faction: "BAF", type: "Air assault" },
-    "Queen's Royal Hussars Battle Group": { faction: "BAF", type: "Armored" },
-    "3rd Division Battle Group": { faction: "BAF", type: "Combined Arms" },
-    "Royal Logistics Corps Battle Group": { faction: "BAF", type: "Logistics" },
-  
-    // CAF
-    "3rd Battalion, Royal Canadian Regiment": { faction: "CAF", type: "Air assault" },
-    "Lord Strathcona's Horse Regiment": { faction: "CAF", type: "Armored" },
-    "1 Canadian Mechanized Brigade Group": { faction: "CAF", type: "Combined Arms" },
-    "12th Armored Regiment of Canada": { faction: "CAF", type: "Light Infantry" },
-    "1st Battalion, Royal 22e Régiment": { faction: "CAF", type: "Mechanized" },
-    "6 Canadian Combat Support Brigade": { faction: "CAF", type: "Logistics" },
-  
-    // USA
-    "1st Brigade Combat Team, 82nd Airborne Division": { faction: "USA", type: "Air assault" },
-    "37th Armored Regiment, 1st Armored Division": { faction: "USA", type: "Armored" },
-    "3rd Brigade Combat Team, 1st Infantry Division": { faction: "USA", type: "Combined Arms" },
-    "1st Brigade Combat Team, 10th Mountain Division": { faction: "USA", type: "Light Infantry" },
-    "1st Cavalry Regiment": { faction: "USA", type: "Mechanized" },
-    "2nd Cavalry Stryker Brigade Combat Team": { faction: "USA", type: "Motorized" },
-    "497th Combat Sustainment Support Battalion": { faction: "USA", type: "Logistics" },
-  
-    // USMC
-    "1st Tank Battalion, 1st Marines": { faction: "USMC", type: "Armored" },
-    "31st Marine Expeditionary Unit": { faction: "USMC", type: "Combined Arms" },
-    "1st Marines Regimental Combat Team": { faction: "USMC", type: "Light Infantry" },
-    "3rd Light Armored Recon Battalion": { faction: "USMC", type: "Motorized" },
-    "2nd Marine Logistics Group": { faction: "USMC", type: "Logistics" },
-    "4th Marines Amphibious Ready Group": { faction: "USMC", type: "Amphibious Assault" },
-  
-    // PLA
-    "161st Air Assault Brigade": { faction: "PLA", type: "Air assault" },
-    "195th Heavy Combined Arms Brigade": { faction: "PLA", type: "Armored" },
-    "118th Combined Arms Brigade": { faction: "PLA", type: "Combined Arms" },
-    "149th Mountain Infantry Brigade": { faction: "PLA", type: "Light Infantry" },
-    "4th Medium Combined Arms Battalion": { faction: "PLA", type: "Mechanized" },
-    "112th Medium Combined Arms Brigade": { faction: "PLA", type: "Motorized" },
-    "80th Support Brigade": { faction: "PLA", type: "Logistics" },
-    "14th Amphibious Combined Arms Brigade": { faction: "PLA", type: "Amphibious Assault" },
-  
-    // PLA MC
-    "4th Marine Special Combat Battalion": { faction: "PLA MC", type: "Light Infantry" },
-    "3rd Marine Heavy Battalion": { faction: "PLA MC", type: "Armored" },
-    "5th Marine Combined Arms Brigade": { faction: "PLA MC", type: "Combined Arms" },
-    "7th Marine Medium Battalion": { faction: "PLA MC", type: "Mechanized" },
-    "17th Marine Support Battalion": { faction: "PLA MC", type: "Logistics" },
-  
-    // PLA GF
-    "9th Heavy Combined Arms Battalion": { faction: "PLA GF", type: "Armored" },
-    "14th Amphibious Combined Arms Battalion": { faction: "PLA GF", type: "Combined Arms" },
-    "7th Marine Medium Battalion": { faction: "PLA GF", type: "Motorized" },
-  
-    // VDV
-    "217th Guards Airborne Regiment": { faction: "VDV", type: "Air assault" },
-    "104th Tank Battalion": { faction: "VDV", type: "Armored" },
-    "7th Guards Mountain Air Assault Division": { faction: "VDV", type: "Combined Arms" },
-    "150th Support Battalion": { faction: "VDV", type: "Logistics" },
-  
-    // MEA
-    "91st Air Assault Battalion": { faction: "MEA", type: "Air assault" },
-    "60th Prince Assur Armored Brigade": { faction: "MEA", type: "Armored" },
-    "1st Battalion, Legion of Babylon": { faction: "MEA", type: "Combined Arms" },
-    "4th Border Guards Group": { faction: "MEA", type: "Light Infantry" },
-  
-    // TLF
-    "1st Commando Brigade Battle Group": { faction: "TLF", type: "Air assault" },
-    "4th Armored Brigade Battle Group": { faction: "TLF", type: "Armored" },
-    "1st Army Battle Group": { faction: "TLF", type: "Combined Arms" },
-    "66th Mechanized Infantry Brigade Battle Group": { faction: "TLF", type: "Mechanized" },
-  
-    // INS
-    "Irregular Armored Squadron": { faction: "INS", type: "Armored" },
-    "Irregular Battle Group": { faction: "INS", type: "Combined Arms" },
-    "Irregular Light Infantry": { faction: "INS", type: "Light Infantry" },
-    "Irregular Fire Support Group": { faction: "INS", type: "Combined Arms" },
-  
-    // RGF
-    "6th Separate Tank Brigade": { faction: "RGF", type: "Armored" },
-    "49th Combined Arms Army": { faction: "RGF", type: "Combined Arms" },
-    "205th Separate Motor Rifle Brigade": { faction: "RGF", type: "Mechanized" },
-    "336th Guards Naval Infantry Brigade": { faction: "RGF", type: "Amphibious Assault" },
-    "3rd Motor Rifle Brigade": { faction: "RGF", type: "Motorized"},
-    "78th Detached Logistics Brigade": { faction: "RGF", type: "Logistics"},
+  // ADF
+  "3rd Battalion, Royal Australian Regiment": { faction: "ADF", type: "Air assault" },
+  "3rd Brigade Battle Group": { faction: "ADF", type: "Combined Arms" },
+  "1st Battalion, Royal Australian Regiment": { faction: "ADF", type: "Mechanized" },
 
-    // PMC
-    "Overwatch 6 Patrol Group": { faction: "PMC", type: "Light Infantry"},
-    "Murk Water Air Wing": { faction: "PMC", type: "Air assault"},
-    "Manticore Security Task Force": { faction: "PMC", type: "Combined Arms"}
-  };
-  
+  // BAF
+  "2nd Battalion, Parachute Regiment": { faction: "BAF", type: "Air assault" },
+  "Queen's Royal Hussars Battle Group": { faction: "BAF", type: "Armored" },
+  "3rd Division Battle Group": { faction: "BAF", type: "Combined Arms" },
+  "Royal Logistics Corps Battle Group": { faction: "BAF", type: "Logistics" },
+
+  // CAF
+  "3rd Battalion, Royal Canadian Regiment": { faction: "CAF", type: "Air assault" },
+  "Lord Strathcona's Horse Regiment": { faction: "CAF", type: "Armored" },
+  "1 Canadian Mechanized Brigade Group": { faction: "CAF", type: "Combined Arms" },
+  "12th Armored Regiment of Canada": { faction: "CAF", type: "Light Infantry" },
+  "1st Battalion, Royal 22e Régiment": { faction: "CAF", type: "Mechanized" },
+  "6 Canadian Combat Support Brigade": { faction: "CAF", type: "Logistics" },
+
+  // USA
+  "1st Brigade Combat Team, 82nd Airborne Division": { faction: "USA", type: "Air assault" },
+  "37th Armored Regiment, 1st Armored Division": { faction: "USA", type: "Armored" },
+  "3rd Brigade Combat Team, 1st Infantry Division": { faction: "USA", type: "Combined Arms" },
+  "1st Brigade Combat Team, 10th Mountain Division": { faction: "USA", type: "Light Infantry" },
+  "1st Cavalry Regiment": { faction: "USA", type: "Mechanized" },
+  "2nd Cavalry Stryker Brigade Combat Team": { faction: "USA", type: "Motorized" },
+  "497th Combat Sustainment Support Battalion": { faction: "USA", type: "Logistics" },
+
+  // USMC
+  "1st Tank Battalion, 1st Marines": { faction: "USMC", type: "Armored" },
+  "31st Marine Expeditionary Unit": { faction: "USMC", type: "Combined Arms" },
+  "1st Marines Regimental Combat Team": { faction: "USMC", type: "Light Infantry" },
+  "3rd Light Armored Recon Battalion": { faction: "USMC", type: "Motorized" },
+  "2nd Marine Logistics Group": { faction: "USMC", type: "Logistics" },
+  "4th Marines Amphibious Ready Group": { faction: "USMC", type: "Amphibious Assault" },
+
+  // PLA
+  "161st Air Assault Brigade": { faction: "PLA", type: "Air assault" },
+  "195th Heavy Combined Arms Brigade": { faction: "PLA", type: "Armored" },
+  "118th Combined Arms Brigade": { faction: "PLA", type: "Combined Arms" },
+  "149th Mountain Infantry Brigade": { faction: "PLA", type: "Light Infantry" },
+  "4th Medium Combined Arms Battalion": { faction: "PLA", type: "Mechanized" },
+  "112th Medium Combined Arms Brigade": { faction: "PLA", type: "Motorized" },
+  "80th Support Brigade": { faction: "PLA", type: "Logistics" },
+  "14th Amphibious Combined Arms Brigade": { faction: "PLA", type: "Amphibious Assault" },
+
+  // PLA MC
+  "4th Marine Special Combat Battalion": { faction: "PLA MC", type: "Light Infantry" },
+  "3rd Marine Heavy Battalion": { faction: "PLA MC", type: "Armored" },
+  "5th Marine Combined Arms Brigade": { faction: "PLA MC", type: "Combined Arms" },
+  "7th Marine Medium Battalion": { faction: "PLA MC", type: "Mechanized" },
+  "17th Marine Support Battalion": { faction: "PLA MC", type: "Logistics" },
+
+  // PLA GF
+  "9th Heavy Combined Arms Battalion": { faction: "PLA GF", type: "Armored" },
+  "14th Amphibious Combined Arms Battalion": { faction: "PLA GF", type: "Combined Arms" },
+  "7th Marine Medium Battalion": { faction: "PLA GF", type: "Motorized" },
+
+  // VDV
+  "217th Guards Airborne Regiment": { faction: "VDV", type: "Air assault" },
+  "104th Tank Battalion": { faction: "VDV", type: "Armored" },
+  "7th Guards Mountain Air Assault Division": { faction: "VDV", type: "Combined Arms" },
+  "150th Support Battalion": { faction: "VDV", type: "Logistics" },
+
+  // MEA
+  "91st Air Assault Battalion": { faction: "MEA", type: "Air assault" },
+  "60th Prince Assur Armored Brigade": { faction: "MEA", type: "Armored" },
+  "1st Battalion, Legion of Babylon": { faction: "MEA", type: "Combined Arms" },
+  "4th Border Guards Group": { faction: "MEA", type: "Light Infantry" },
+
+  // TLF
+  "1st Commando Brigade Battle Group": { faction: "TLF", type: "Air assault" },
+  "4th Armored Brigade Battle Group": { faction: "TLF", type: "Armored" },
+  "1st Army Battle Group": { faction: "TLF", type: "Combined Arms" },
+  "66th Mechanized Infantry Brigade Battle Group": { faction: "TLF", type: "Mechanized" },
+
+  // INS
+  "Irregular Armored Squadron": { faction: "INS", type: "Armored" },
+  "Irregular Battle Group": { faction: "INS", type: "Combined Arms" },
+  "Irregular Light Infantry": { faction: "INS", type: "Light Infantry" },
+  "Irregular Fire Support Group": { faction: "INS", type: "Combined Arms" },
+
+  // RGF
+  "6th Separate Tank Brigade": { faction: "RGF", type: "Armored" },
+  "49th Combined Arms Army": { faction: "RGF", type: "Combined Arms" },
+  "205th Separate Motor Rifle Brigade": { faction: "RGF", type: "Mechanized" },
+  "336th Guards Naval Infantry Brigade": { faction: "RGF", type: "Amphibious Assault" },
+  "3rd Motor Rifle Brigade": { faction: "RGF", type: "Motorized" },
+  "78th Detached Logistics Brigade": { faction: "RGF", type: "Logistics" },
+
+  // PMC
+  "Overwatch 6 Patrol Group": { faction: "PMC", type: "Light Infantry" },
+  "Murk Water Air Wing": { faction: "PMC", type: "Air assault" },
+  "Manticore Security Task Force": { faction: "PMC", type: "Combined Arms" }
+};
